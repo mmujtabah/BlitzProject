9d8e842015373c2728927ba91319e031fecd9ec3
Muhammad Mujtaba Hussain <mmujtabah1599@gmail.com>
2023-11-16 21:36:36 +0500

    Update Consolas.cpp

--- Consolas.cpp
+++ Consolas.cpp
@@ -37,16 +37,55 @@ void InitializeBoard(char board[8][8], int rows, int cols){
     }
 }
 
-int checks(char board[8][8], int rows, int cols){
+int scoreCalculator(char gem, int gemCount){
+    int scoreCount = 0;
+    switch (gem)
+    {
+    case 'W':
+        scoreCount = 5;
+        break;
+    case 'B':
+        scoreCount = 6;
+        break;
+    case 'O':
+        scoreCount = 7;
+        break;
+    case 'R':
+        scoreCount = 8;
+        break;
+    case 'P':
+        scoreCount = 9;
+        break;
+    case 'Y':
+        scoreCount = 10;
+        break;
+    case 'G':
+        scoreCount = 11;
+        break;
+    default:
+        break;
+    }
+    scoreCount *= gemCount;
+    return scoreCount;
+}
+
+
+void checks(char board[8][8], int rows, int cols){
     bool found = false;
     //Row Check
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++){
-            if (j + 1 < rows && j + 2 < cols && j + 3 < rows){
-                if (board[i][j] == board[i][j+1] && board[i][j] == board[i][j+2]){
-                    found = true;
-                    return 1;
+            int index1 = j + 1;
+            int index2 = j + 2;
+            int index3 = j + 3;
+            if (index1 < rows  && index2 < rows){
+                if (index3 < rows){
+                    if (board[i][j] == board[i][index1] && board[i][index1] == board[i][index2] && board[i][index3]){
+                        
+                    }
                 }
+            }else{
+
             }
         }
     }
@@ -56,12 +95,10 @@ int checks(char board[8][8], int rows, int cols){
             if (j + 1 < rows && j + 2  && j + 3 < cols){
                 if (board[j][i] == board[j+1][i] && board[j][i] == board[j+2][i]){
                     found = true;
-                    return 1;
                 }
             }
         }
     }
-    return 0;
 }
 
 void Display(char board[8][8], int rows, int cols) {
