4fd2a28d8a1d555c688a1f10c8d90a5f097e8aeb
Muhammad Mujtaba Hussain <mmujtabah1599@gmail.com>
2023-11-19 10:52:31 +0500

    Update

--- main.cpp
+++ main.cpp
@@ -4,20 +4,102 @@
 #include <iostream>
 #include <iomanip>
 
-int main() {
+static sf::RectangleShape board[8][8];
+static sf::Texture textures[7];
+static sf::Sprite sprites[8][8];
+
+int highlightedRow = 0;
+int highlightedCol = 0;
+
+// Initialize random seed
+void RandomSeed()
+{
+    std::srand(static_cast<unsigned int>(std::time(0)));
+}
+
+//Stores random images to textures and sets the sprite textures
+void randTexScaleSprites(sf::Texture textures[], sf::Sprite sprites[][8], sf::RectangleShape board[][8], float cellSize)
+{
+    //Stores images in textures array
+    for (int i = 0; i <= 6; ++i)
+    {
+        std::string filename = "image" + std::to_string(i) + ".png"; // Adjust filenames as needed
+        if (!textures[i].loadFromFile(filename))
+        {
+            std::cerr << "Failed to load texture: " << filename << std::endl;
+            return;
+        }
+    }
+
+    // Assign random textures to each sprite and set scale
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            board[i][j].setSize(sf::Vector2f(cellSize, cellSize));
+            board[i][j].setPosition((i + 7) * cellSize, (j + 0.2) * cellSize);
+            if ((i + j) % 2 == 0)
+            {
+                board[i][j].setFillColor(sf::Color(43, 42, 42)); // Grey
+            }
+            else
+            {
+                board[i][j].setFillColor(sf::Color(0, 0, 0)); // Black
+            }
+
+            int randomTextureIndex = rand() % 7;
+            sprites[i][j].setTexture(textures[randomTextureIndex]);
+
+            // Set the scale of the sprites
+            float scale = cellSize / static_cast<float>(textures[randomTextureIndex].getSize().x);
+            sprites[i][j].setScale(scale, scale - 0.01f);
+
+            // Center the sprite within the board block
+            float xOffset = (cellSize - sprites[i][j].getGlobalBounds().width) / 2.0f;
+            float yOffset = (cellSize - sprites[i][j].getGlobalBounds().height) / 2.0f;
+            sprites[i][j].setPosition(board[i][j].getPosition().x + xOffset, board[i][j].getPosition().y + yOffset);
+        }
+    }
+}
+
+void moveHighlight(sf::Keyboard::Key key)
+{
+    switch (key)
+    {
+    case sf::Keyboard::Left:
+        highlightedRow = std::max(0, highlightedRow - 1);
+        break;
+    case sf::Keyboard::Right:
+        highlightedRow = std::min(7, highlightedRow + 1);
+        break;
+    case sf::Keyboard::Up:
+        highlightedCol = std::max(0, highlightedCol - 1);
+        break;
+    case sf::Keyboard::Down:
+        highlightedCol = std::min(7, highlightedCol + 1);
+        break;
+    default:
+        break;
+    }
+}
+
+int main()
+{
     // Set up the window
     sf::RenderWindow window(sf::VideoMode(1000, 600), "Blitz");
 
     // Set up the font for text
     sf::Font font;
-    if (!font.loadFromFile("font.ttf")) {
+    if (!font.loadFromFile("font.ttf"))
+    {
         std::cerr << "Failed to load font!" << std::endl;
         return -1;
     }
 
     // Set up the bold font for title
     sf::Font fontBold;
-    if (!fontBold.loadFromFile("fontBold.otf")) {
+    if (!fontBold.loadFromFile("fontBold.otf"))
+    {
         std::cerr << "Failed to load bold font!" << std::endl;
         return -1;
     }
@@ -44,45 +126,11 @@ int main() {
     gameInfo.setCharacterSize(25);
     gameInfo.setFillColor(sf::Color::Black);
 
+    RandomSeed();
+
     // Set up the board
-    static sf::RectangleShape board[8][8];
-    sf::Texture textures[7];
-    sf::Sprite sprites[8][8];
     const float cellSize = 62.0f;
-
-    // Initialize random seed
-    std::srand(static_cast<unsigned int>(std::time(0)));
-
-    for (int i = 0; i < 6; ++i) {
-        std::string filename = "image" + std::to_string(i) + ".png";  // Adjust filenames as needed
-        if (!textures[i].loadFromFile(filename)) {
-            std::cerr << "Failed to load texture: " << filename << std::endl;
-            return -1;
-        }
-    }
-
-    // Assign random textures to each sprite and set scale
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            board[i][j].setSize(sf::Vector2f(cellSize, cellSize));
-            board[i][j].setPosition((i + 7) * cellSize, (j + 0.2) * cellSize);
-            if ((i + j) % 2 == 0) {
-                board[i][j].setFillColor(sf::Color(43, 42, 42)); // Grey
-            }
-            else {
-                board[i][j].setFillColor(sf::Color(0, 0, 0)); // Black
-            }
-
-            int randomTextureIndex = std::rand() % 6;
-            sprites[i][j].setTexture(textures[randomTextureIndex]);
-
-            // Set the scale of the sprites
-            float scale = cellSize / static_cast<float>(textures[randomTextureIndex].getSize().x);
-            sprites[i][j].setScale(scale, scale);
-
-            sprites[i][j].setPosition(board[i][j].getPosition());
-        }
-    }
+    randTexScaleSprites(textures, sprites, board, cellSize);
 
     // Set up the clock
     sf::Clock clock;
@@ -91,37 +139,26 @@ int main() {
     int score = 0;
 
     // Game loop
-    while (window.isOpen()) {
+    while (window.isOpen())
+    {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
             }
-            else if (event.type == sf::Event::MouseButtonPressed) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    // Handle mouse click
-                    // Add logic to detect the clicked block and perform swapping
-                    // For simplicity, let's just change the texture of the clicked block
-                    int mouseX = event.mouseButton.x;
-                    int mouseY = event.mouseButton.y;
-
-                    for (int i = 0; i < 8; ++i) {
-                        for (int j = 0; j < 8; ++j) {
-                            if (board[i][j].getGlobalBounds().contains(static_cast<float>(mouseX), static_cast<float>(mouseY))) {
-                                // Assign a new random texture to the clicked block
-                                int randomTextureIndex = std::rand() % 7;
-                                sprites[i][j].setTexture(textures[randomTextureIndex]);
-                            }
-                        }
-                    }
-                }
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                moveHighlight(event.key.code);
             }
         }
 
         // Update time and score
         sf::Time elapsed = clock.getElapsedTime();
         int remainingSeconds = 60 - static_cast<int>(elapsed.asSeconds());
-        if (remainingSeconds < 0) {
+        if (remainingSeconds < 0)
+        {
             remainingSeconds = 0;
         }
 
@@ -135,16 +172,16 @@ int main() {
         gameInfo.setString("Game developed by M.Mujtaba and Harris Tabassum");
 
         // Adjust positions for separation
-        gameTitle.setPosition(10.0f, 45.0f); //Adjust as needed
-        timeText.setPosition(10.0f, 220.0f);  // Adjust as needed
-        scoreText.setPosition(10.0f, 320.0f);  // Adjust as needed
-        gameInfo.setPosition(50.0f, 550.0f);  // Adjust as needed
+        gameTitle.setPosition(10.0f, 45.0f);    // Adjust as needed
+        timeText.setPosition(10.0f, 220.0f);     // Adjust as needed
+        scoreText.setPosition(10.0f, 320.0f);    // Adjust as needed
+        gameInfo.setPosition(50.0f, 550.0f);    // Adjust as needed
 
         // Clear the window
         window.clear();
 
         // Set the background color
-        window.clear(sf::Color::White);  // Change to your desired background color
+        window.clear(sf::Color::White); // Change to your desired background color
 
         // Draw the time and score text
         window.draw(gameTitle);
@@ -152,19 +189,32 @@ int main() {
         window.draw(scoreText);
         window.draw(gameInfo);
 
-        // Draw the board
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
+        // Draw the board with highlight
+        for (int i = 0; i < 8; ++i)
+        {
+            for (int j = 0; j < 8; ++j)
+            {
                 window.draw(board[i][j]);
                 window.draw(sprites[i][j]);
             }
         }
 
+        // Draw the highlight with transparency and borders
+        sf::RectangleShape highlight;
+        highlight.setSize(sf::Vector2f(cellSize, cellSize));
+        highlight.setPosition(board[highlightedRow][highlightedCol].getPosition());
+        highlight.setFillColor(sf::Color(255, 255, 0, 0)); // Yellow with 50% transparency
+        highlight.setOutlineThickness(2.0f);                 // Border thickness
+        highlight.setOutlineColor(sf::Color::Yellow);        // Border color
+        window.draw(highlight);
+
         // Display the content
         window.display();
 
-        // Exit the program when time is up
-        if (remainingSeconds == 0) {
+        // Exit the program after a delay when time is up
+        if (remainingSeconds == 0)
+        {
+            sf::sleep(sf::seconds(2)); // Add a 2-second delay
             window.close();
         }
     }
